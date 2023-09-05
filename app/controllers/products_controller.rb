3f445e039c3f50d127b11bb9504ccf0f1c116f1b
Adélia Mara <adeliamara13@gmail.com>
2023-09-05 13:25:59 -0300

    refactor: create methods to order, search e filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,14 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
-
+  
   # GET /products or /products.json
-  def index
-    @products = Product.all
-  end
+def index
+  @products = Product.all
+
+  apply_search
+  apply_filter
+  apply_order
+end
 
   # GET /products/1 or /products/1.json
   def show
@@ -36,7 +40,7 @@ class ProductsController < ApplicationController
 
   # PATCH/PUT /products/1 or /products/1.json
   def update
-    respond_to do |format|
+    respond_to do |format|Q
       if @product.update(product_params)
         format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @product }
@@ -57,8 +61,34 @@ class ProductsController < ApplicationController
     end
   end
 
+
   private
-    # Use callbacks to share common setup or constraints between actions.
+
+    def apply_search 
+      if params[:search].present?
+        @products = @products.search(params[:search])
+      end
+    end
+
+    def apply_filter
+      if params[:status].present?
+        @products = @products.status(params[:status])
+      end
+
+      if params[:min_profitabily_rate].present?
+        @products = @products.profitabily_rate_mim(params[:min_profitabily_rate])
+      end
+
+      @products
+    end
+
+    def apply_order
+      order_by = params[:order] || 'name'
+      direction = params[:direction] || 'asc'
+    
+      @products = @products.order("#{order_by} #{direction}")
+    end
+      # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
     end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,29 +1,33 @@
 class Product < ApplicationRecord
 
-    STATUS_OPTIONS = ['Disponível', 'Indisponível']
+  STATUS_OPTIONS = ['Disponível', 'Indisponível']
 
-    validates :status, inclusion: { in: STATUS_OPTIONS }
-    validates :name, length: { maximum: 32 }
-    validates :destination,length:  { maximum: 180 }
-    validates :profitabily_rate, inclusion: { in: 1..20 }
-    validates :minimum_period_for_realization, inclusion: { in: 0..48 }
-    validates :administration_fee, numericality: { only_integer: false }
-    validates :due_date, presence: true
-    validates :daily_liquidity, numericality: { only_integer: false }
+  validates :status, inclusion: { in: STATUS_OPTIONS }
+  validates :name, length: { maximum: 32 }
+  validates :destination,length:  { maximum: 180 }
+  validates :profitabily_rate, inclusion: { in: 1..20 }
+  validates :minimum_period_for_realization, inclusion: { in: 0..48 }
+  validates :administration_fee, numericality: { only_integer: false }
+  validates :due_date, presence: true
+  validates :daily_liquidity, numericality: { only_integer: false }
+
+  validate :due_date_must_be_after_minimum_period
+
+  scope :status, ->(status) { where('status = ?',status )}
+  scope :profitabily_rate_mim, ->(min_profitabily_rate) { where('profitabily_rate > ?', min_profitabily_rate) }
+  scope :search, ->(name) { where('name LIKE ?', "%#{name}%") }
 
 
-    validate :due_date_must_be_after_minimum_period
 
-  
-    private
-  
-    def due_date_must_be_after_minimum_period
-      departure_date = created_at.blank? ? Date.today : created_at.to_date
+  private
 
-      minimum_due_date = departure_date + minimum_period_for_realization.months
-  
-      if due_date < minimum_due_date
-        errors.add(:due_date, "deve ser pelo menos #{minimum_period_for_realization} meses após a data de criação")
-      end
+  def due_date_must_be_after_minimum_period
+    departure_date = created_at.blank? ? Date.today : created_at.to_date
+
+    minimum_due_date = departure_date + minimum_period_for_realization.months
+
+    if due_date < minimum_due_date
+      errors.add(:due_date, "deve ser pelo menos #{minimum_period_for_realization} meses após a data de criação")
     end
+  end
 end
